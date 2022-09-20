1c0babb760f0cb403cc888389c24ac77cd59862f
Gao Wei <gwval3344@foxmail.com>
2022-09-20 20:41:38 +0800

    更新相对路径

--- main.ts
+++ main.ts
@@ -1,137 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TAbstractFile } from 'obsidian';
+import { posix as path } from 'path';
 
-// Remember to rename these classes and interfaces!
+export default class UpdateRelativeLinksPlugin extends Plugin {
+    async onload() {
+        const { metadataCache, vault } = this.app;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+        this.addCommand({
+            id: 'update-all-relative-links',
+            name: 'Update all relative links',
+            callback() {
+                const promises = vault.getMarkdownFiles().map((file, idx, arr) => {
+                    console.log(`start ${idx + 1}/${arr.length}`);
+                    return replace(file, false).finally(() => console.log(`end ${idx + 1}/${arr.length}`));
+                });
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+                Promise.all(promises).finally(() => console.log('all done'));
+            }
+        });
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        this.registerEvent(vault.on('rename', (file, oldPath) => {
+            if (!oldPath
+                || !file.path.toLocaleLowerCase().endsWith('.md')
+                || file.parent.path === path.dirname(oldPath)) {
+                return;
+            }
 
-	async onload() {
-		await this.loadSettings();
+            setTimeout(() => replace(file, true), 100);
+        }));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        function replace(file: TAbstractFile, notice: boolean) {
+            const metadata = metadataCache.getCache(file.path);
+            const links = [...(metadata?.links ?? []), ...(metadata?.embeds ?? [])];
+            const replacePairs = links.map(({ link, original }) => {
+                const linkFile = metadataCache.getFirstLinkpathDest(link, file.path);
+                
+                if (!linkFile) {
+                    return null;
+                }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+                const newLink = path.relative(file.parent.path, linkFile.path);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                if (link === newLink) {
+                    return null;
+                }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+                const newOriginal = original.replace(link, newLink);
+                return [original, newOriginal];
+            }).filter(pair => pair);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+            if (!replacePairs?.length) {
+                return Promise.resolve();
+            }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+            return vault.adapter.read(file.path).then(content => {
+                const replacedContent = replacePairs.reduce((tmpContent, pair) => {
+                    return pair?.length === 2 ? tmpContent.replace(pair[0], pair[1]) : tmpContent;
+                }, content);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+                return vault.adapter.write(file.path, replacedContent);
+            }).then(() => {
+                const msg = `Update ${replacePairs.length} links in ${file.path}.`;
+                console.log(msg);
+                if (notice) {
+                    new Notice(msg)
+                }
+            }).catch(err => {
+                console.error(err);
+                if (notice) {
+                    new Notice('Update links error, see console.');
+                }
+            });
+        }
+    }
 }
